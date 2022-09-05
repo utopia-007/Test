bb3f97372ce12addc24432f7eb8182f71a6285a9
utopia <wschgjbts@163.com>
2022-09-05 12:19:32 +0800

    ;aldfk;a

---
+++ Test/CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CodeFile1;
+class CodeFile1
+{
+static void Main(string[] args)
+{
+    short a;
+        int b, x;
+    double c;
+
+            //初始化变量a
+            a = 10;
+    //读取键盘输入，并转换为整数
+    Console.WriteLine("请输入一个数字：");
+    b = Convert.ToInt32(Console.ReadLine());
+    c = a + b;
+    //输出
+    Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+    Console.ReadLine();
+}
+} 
+  
---
+++ Test/CodeFile2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CodeFile2;
+class CodeFile2{
+
+static void Main(string[] args)
+{
+        Console.WriteLine("Helloword");
+
+}
+}
